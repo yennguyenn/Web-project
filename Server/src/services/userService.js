9daa69f5fc260a9124a31b4fae9ea68751fc12a8
mptx4869 <dangnhanhoa2305@gmail.com>
2024-12-17 21:02:13 +0700

    complete updateUser  API

--- Server/src/services/userService.js
+++ Server/src/services/userService.js
@@ -49,10 +49,17 @@ let createNewUser = (data) => {
                 gender: data.gender == '1' ? true : false,
                 roleId: data.roleId,
             })
-            let user = await db.User.findOne({
-                where: { email: data.email }
+            let user = await getUserByEmail(data.email)
+            if (user) resolve({
+                errCode: 0,
+                messagse: "create success",
+                userData: user
             })
-            resolve(user)
+            else
+                resolve({
+                    errCode: 2,
+                    message: "create fail"
+            })
         } catch (e) {
             reject(e)
         }
@@ -67,20 +74,20 @@ let handleLogin = (email, password) => {
             let user = await getUserByEmail(email)
             let userData = {}
             if (user) {
-                if( bcryptjs.compareSync(password,user.password)){
-                    userData.errCode = 0 ,
-                    delete user.password
+                if (bcryptjs.compareSync(password, user.password)) {
+                    userData.errCode = 0,
+                        delete user.password
                     userData.errMessage = 'OK',
-                    userData.user = user
-                }else{
-                    userData.errCode = 2 ,
-                    userData.errCode = "wrong password"
+                        userData.user = user
+                } else {
+                    userData.errCode = 2,
+                        userData.errCode = "wrong password"
                 }
-                
+
             } else {
                 userData.errCode = 1
                 userData.errMessage = "your email does't exit"
-                
+
             }
 
             resolve(userData)
@@ -90,66 +97,88 @@ let handleLogin = (email, password) => {
     })
 }
 
-let deleteUser = (userId) =>{
-    return new Promise(async (resolve,reject)=>{
+let deleteUser = (userId) => {
+    return new Promise(async (resolve, reject) => {
         try {
             console.log("run deleteUser in userService");
             let user = await getUserById(userId)
 
-            if(!user) resolve({
-                errCode : 2,
+            if (!user) resolve({
+                errCode: 2,
                 errMessage: "user does not exist"
             })
 
-            let isSuccesss =  await db.User.destroy({
-                where:{userId : userId} 
+            let isSuccesss = await db.User.destroy({
+                where: { userId: userId }
             })
 
-            if(isSuccesss) resolve({
-                errCode:0,
+            if (isSuccesss) resolve({
+                errCode: 0,
                 messagse: "delete success"
             })
             else
                 resolve({
-                    error:2,
-                    errMessage:"delete fail"
+                    error: 2,
+                    errMessage: "delete fail"
                 })
-            
-        } catch (e ) {
+
+        } catch (e) {
             reject(e)
         }
     })
 }
 
-let updateUser = (userData) =>{
+let updateUser = (userData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log("run updateUser in UserService");
+            let user = await getUserById(userData.userId)
 
+            if (!user) resolve({
+                errCode: 1,
+                errMessage: "user does not exist "
+            })
+            delete userData.password
+            await db.User.update(userData, {
+                where: { userId: userData.userId }
+            })
+
+            resolve({
+                errCode: 0,
+                messagse: "update Success"
+            })
+
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
 // suport method 
 
-let getUserById = (id)=>{
-    return new Promise(async(resolve,reject) => {
+let getUserById = (id) => {
+    return new Promise(async (resolve, reject) => {
         try {
             const date = Date.now()
-            console.log("run in getUserById at userService " +date);
-            
+            console.log("run in getUserById at userService " + date);
+
             let user = await db.User.findOne({
-                where:{userId : id},
-                raw:true
+                where: { userId: id },
+                raw: true
             })
             resolve(user)
         } catch (error) {
             reject(error)
         }
-        
+
     })
 }
 
-let comparePassword = (password)=>{
-    return new Promise( async(resolve,reject) =>{
+let comparePassword = (password) => {
+    return new Promise(async (resolve, reject) => {
         try {
             let hashPassword = await hashUserPassword(password)
-           
+
         } catch (error) {
             reject(error)
         }
@@ -162,8 +191,8 @@ let getUserByEmail = (email) => {
             let user = await db.User.findOne({
                 where: { email: email },
                 attributes: {
-                    include: ['email','roleId'],
-                },  
+                    include: ['email', 'roleId'],
+                },
                 raw: true,
             })
             console.log(user);
@@ -181,7 +210,7 @@ let findUserByPhonenumber = (phonenumber) => {
                 where: { phonenumber: phonenumber },
                 raw: true,
             })
-            resolve (user)
+            resolve(user)
         } catch (error) {
             reject(error)
         }
@@ -203,7 +232,7 @@ let hashUserPassword = (password) => {
 module.exports = {
     createNewUser: createNewUser,
     handleLogin: handleLogin,
-    getUserById:getUserById,
-    deleteUser : deleteUser,
-    updateUser : updateUser,
+    getUserById: getUserById,
+    deleteUser: deleteUser,
+    updateUser: updateUser,
 }
