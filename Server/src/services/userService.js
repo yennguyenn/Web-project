8b361c1a8f86357efc7b8f1c98b5289246ffc2cc
mptx4869 <dangnhanhoa2305@gmail.com>
2024-12-18 17:28:20 +0700

    fix exp error"

--- Server/src/services/userService.js
+++ Server/src/services/userService.js
@@ -74,16 +74,13 @@ let handleLogin = (email, password) => {
                     let token =  await JwtAction.createJWT({
                         email : email,
                         roleId : user.roleId,
-                        exp:process.env.JWT_EXPIRES_In,
+                        exp:3600,
                     })
                     userData.errCode = 0,
                     delete user.password
                     userData.errMessage = 'OK';
                     userData.token = token
-                    userData.user = user
-
-                    console.log(JwtAction.verifyToken(token));
-                    
+                    userData.user = user    
 
                 } else {
                     userData.errCode = 2,
